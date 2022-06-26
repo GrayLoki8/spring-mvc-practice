5ed8a88aa2c97679656de33cc31582c3d5dac86d
Gray Loki <skuc090@gmail.com>
2022-06-26 21:31:36 +0300

    test @RequestParam

--- src/main/java/ua/grayloki8/spring/controllers/FirstController.java
+++ src/main/java/ua/grayloki8/spring/controllers/FirstController.java
@@ -2,15 +2,14 @@ package ua.grayloki8.spring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name=request.getParameter("name");
-        String surname=request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name",required = false) String name,@RequestParam(value = "surname",required = false)String surname){
 
         System.out.println("Hello "+name+" "+surname);
         return "first/hello";
