706ff3cbdf9ba7765e9f8aa18e9f06fa0a1bbafc
Gray Loki <skuc090@gmail.com>
2022-06-26 18:55:12 +0300

    create first controllers

---
+++ src/main/java/ua/grayloki8/spring/controllers/FirstController.java
@@ -0,0 +1,16 @@
+package ua.grayloki8.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage(){
+        return "first/hello";
+    }
+    @GetMapping("/goodbye")
+    public String goodByePage(){
+        return "first/goodbye";
+    }
+}
---
+++ src/main/java/ua/grayloki8/spring/controllers/SecondController.java
@@ -0,0 +1,10 @@
+package ua.grayloki8.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/exit")
+    public String exit(){return "second/exit";}
+}
