6b0a00cd5eb2bf4707d89f759446deb631512c71
Gray Loki <skuc090@gmail.com>
2022-06-27 15:28:23 +0300

    create functional for showing people what in list

---
+++ src/main/java/ua/grayloki8/spring/dao/PersonDAO.java
@@ -0,0 +1,25 @@
+package ua.grayloki8.spring.dao;
+
+import org.springframework.stereotype.Component;
+import ua.grayloki8.spring.modals.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+    {
+        people=new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT,"Loki"));
+        people.add(new Person(++PEOPLE_COUNT,"Bob"));
+        people.add(new Person(++PEOPLE_COUNT,"Tor"));
+        people.add(new Person(++PEOPLE_COUNT,"Odin"));
+    }
+    public List<Person> index(){
+        return people;
+    }
+    public Person show(final int id){
+        return people.stream().filter(people->people.getId()==id).findAny().orElse(null);
+    }
+}
